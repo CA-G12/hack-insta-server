963462f9181dad6d7652ccc8e10f244e7ede95fd
fadezak100 <fadyzaqut@gmail.com>
2022-09-23 16:39:55 +0300

    complete access token endpoint

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const axios = require('axios');
 const curl = require('curl');
 const FormData = require('form-data');
-const { urlencoded } = require('express');
 const fs = require('fs');
 const { join } = require('path');
-
+const fetch = require('node-fetch')
 require('dotenv').config();
 
 const app = express();
@@ -18,7 +17,7 @@ app.get('/', (req, res) => {
 
 app.post('/remove-bg', (req, res) => {
 
-    const {image } = req.body;
+    const { image_url } = req.body;
     const formData = new FormData();
     formData.append('size', 'auto');
     formData.append('image_url', image);
@@ -30,7 +29,7 @@ app.post('/remove-bg', (req, res) => {
       responseType: 'arraybuffer',
       headers: {
         ...formData.getHeaders(),
-        'X-Api-Key': 'fcSUHGvw72Vc8M3k2QZzphJw',
+        'X-Api-Key': process.env.rb_key,
       },
       encoding: null
     })
@@ -49,6 +48,27 @@ app.post('/test-posting', (req, res) => {
   const { name } = req.body; 
   res.json({msg: `hi ${name}`});
 })
+
+app.post('/authorize_token', (req, res) => {
+  const { code } = req.body;
+  const url = 'https://api.instagram.com/oauth/access_token';
+
+  const form = new FormData();
+  form.append('client_id', process.env.client_id);
+  form.append('client_secret', process.env.client_server);
+  form.append('grant_type', 'authorization_code');
+  form.append('redirect_uri', 'https://localhost:3000/');
+  form.append('code', code);
+
+  curl.post(url, form, {headers: {
+  ...form.getHeaders(),
+  }}, (err, data) => {
+      if (!err) res.json(data.body)
+      else res.json(err);
+  });
+
+});
+
 
 app.listen(process.env.PORT || 4000, () => {
     console.log('Server is running on port 4000');    
